85c1a0342feee7b3d803f2ee1d1bbf9c2b222fb0
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-08-05 20:39:42 -0300

    aula 05-08

--- src/Ars.jsx
+++ src/Ars.jsx
@@ -1,11 +1,11 @@
-import style from './App.module.css'
+import style from './Ars.module.css'
 
 function App() {
 
   return (
     <div>
       <h1>Projeto Vite - H1</h1>
-      <h2 className={style.t2}>Exemplo h2</h2>
+      <h2 className={style.t2}>Exemplo h2 - xxx</h2>
       <h3>Exemplo h3</h3>
       <p>Exemplo parágrafo</p>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/reset.css'
+import './css/global.css'
 import App from './App.jsx'
+import Ars from './Ars.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Ars />
   </StrictMode>,
 )
